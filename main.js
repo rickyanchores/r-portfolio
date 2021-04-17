4cf8e4ccca5d5286def178c1aca16a790dce0c58
RickyAnchores <ricky23anchores@gmail.com>
2021-04-18 00:12:09 +0100

    upd navbar

---
+++ main.js
@@ -0,0 +1,8 @@
+// NAV RESPONSIVE
+
+const hamburger = document.querySelector('#hamburger');
+const navUL = document.querySelector('#nav-ul');
+
+hamburger.addEventListener('click', () =>{
+  navUL.classList.toggle('open');
+});
