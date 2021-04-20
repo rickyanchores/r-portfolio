68255ece0dcc92fbcc05c9906fc8dc572dd7f2a1
RickyAnchores <ricky23anchores@gmail.com>
2021-04-20 19:57:21 +0100

    added dark mode setup

--- main.js
+++ main.js
@@ -6,3 +6,14 @@ const navUL = document.querySelector('#nav-ul');
 hamburger.addEventListener('click', () =>{
   navUL.classList.toggle('open');
 });
+
+
+//LOGO -> Dark Mode
+
+let logo = document.querySelector('#logo');
+
+logo.addEventListener('dblclick', () => {
+  document.body.style.background = "black";
+  document.body.style.color = "whitesmoke";
+  document.body.style.transition = ".5s ease-in";
+})
