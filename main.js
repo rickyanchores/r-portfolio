575122b8f0b47f271d890b0294ec4e1c57ef415d
RickyAnchores <ricky23anchores@gmail.com>
2021-04-21 18:43:59 +0100

    main js upd

--- main.js
+++ main.js
@@ -8,12 +8,23 @@ hamburger.addEventListener('click', () =>{
 });
 
 
-//LOGO -> Dark Mode
+//LOGO 
 
 let logo = document.querySelector('#logo');
 
-logo.addEventListener('dblclick', () => {
+
+// COLOR FUNCTIONS 
+
+//DARK
+
+logo.addEventListener('click', () => {
   document.body.style.background = "black";
   document.body.style.color = "whitesmoke";
+  document.body.style.transition = ".5s ease-in";
+})
+
+logo.addEventListener('dblclick', () => {
+  document.body.style.background = "";
+  document.body.style.color = "";
   document.body.style.transition = ".5s ease-in";
 })
